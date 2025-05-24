967cf25c3afda61eced66cde4511c0f19629dbe6
anvesh-527 <anveshkomuravelli@gmail.com>
2025-05-24 13:12:36 +0530

    Event Handling

--- lec67/vite-project/src/App.jsx
+++ lec67/vite-project/src/App.jsx
@@ -4,30 +4,54 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  
+  function handleClick(e) {
+    alert("I am clicked");
+  }
+  function handleMouseOver() {
+    alert("Para per mouse hai");
+  }
+  function handleInputChange(e) {
+    console.log("The value in the input has been changed: " + e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Writing custom behaviour
+    alert("Form has been submitted");
+  } 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* This is called as immediate invocation.
+    This will be executed whenever the component is rendered (or) the page is refreshed even without clicking it. */}
+    <button onClick={alert("Button clicked")}>
+      Click me
+    </button>
+    {/* Instead we should write 
+    Comment the above one to observe it correctly.*/}
+    <button onClick={()=>{alert("Button clicked")}}>
+      Click me
+    </button>
+
+    {/* <form onSubmit={handleSubmit} >
+      <input
+        placeholder="Type something here...."
+        class="input"
+        name="text"
+        type="text"
+        onChange={(e) => {handleInputChange(e)}}
+      />
+      <button type='submit' className='button'>Submit</button>
+    </form> */}
+
+      {/* <p onMouseOver={handleMouseOver} style={{border:"2px solid black",color:"red"}} >I am para</p> */}
+
+      
+
+      {/* <button onClick={handleClick} className='button'>
+        Click me
+      </button> */}
+      
     </>
   )
 }
